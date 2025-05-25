4b996fcf6685e22beb09f7354338d85153c7a49a
ppre1ude <cjh5110@naver.com>
2025-05-25 15:51:03 +0900

    대시보드, pokecard, pokelist 구현 + mock.js 불러오기

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Dashboard from "../components/Dashboard";
+import PokemonList from "../components/PokemonList";
+import MOCK_DATA from "../mock";
+
+const DexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+
 export default function Dex() {
-  return <div>근본 포켓몬 도감 호출</div>;
+  const [selectedPokemon, setSelectedPokemon] = useState([]);
+  return (
+    <DexContainer>
+      <Dashboard selectedPokemon={selectedPokemon} />
+      <PokemonList pokemonList={MOCK_DATA} />
+    </DexContainer>
+  );
 }
